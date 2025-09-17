8f53c9a05f89cdfe965363e1ba8f8cfbcd8b75c8
nelyaklyusa <nelya.klyusa@gmail.com>
2025-09-17 20:45:34 +0300

    update 404

--- src/features/static/ViewError404.vue
+++ src/features/static/ViewError404.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { onBeforeMount } from 'vue';
 import { useGlobalLoader } from 'UiKit/store/useGlobalLoader';
-
+import env from 'InvestCommon/domain/config/env';
 
 useGlobalLoader().show();
 
+
 onBeforeMount(() => {
   useGlobalLoader().show();
-  window.location.reload();
+  const dashboardBaseUrl = env.FRONTEND_URL_DASHBOARD;
+  if (dashboardBaseUrl) {
+    const { pathname, search, hash } = window.location;
+    const needsDashboardPrefix = !pathname.startsWith('/dashboard');
+    const preservedPath = `${needsDashboardPrefix ? '/dashboard' : ''}${pathname}${search}${hash}`;
+    const targetUrl = new URL(preservedPath, dashboardBaseUrl).toString();
+    window.location.replace(targetUrl);
+  }
+  // useGlobalLoader().hide();
 });
 </script>
 
