ca070aab28576bbbeb0cd1b5aa6f97a0394d16d1
mikimakimiki <mikhailiakubovich@gmail.com>
2025-09-24 16:40:28 +0400

    pwa store and footer optimized

---
+++ src/domain/pwa/store/useAppStateStore.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+import { installPwaNoZoomGuards } from '../disableZoom';
+import { isPwaMobile } from '../pwaDetector';
+
+export const useAppStateStore = defineStore('appState', {
+  state: () => ({
+    isPwa: false,
+    isMobile: false,
+  }),
+  actions: {
+    initializeAppState() {
+      this.isMobile = isPwaMobile();
+      if (this.isMobile) {
+        this.isPwa = true;
+        installPwaNoZoomGuards();
+      }
+    },
+  },
+});
